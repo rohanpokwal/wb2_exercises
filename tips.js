47fdc5115f6a0227022415dade48707af37f6ccd
Rohan Pokwal <rohan.pokwal@gmail.com>
2024-04-24 10:55:35 -0500

    Completed tips.JS

---
+++ tips.js
@@ -0,0 +1,12 @@
+//variables create as inputs for calculation
+let foodCost = 79.25;
+let tax = 6.54;
+let tip = 12.0;
+
+//do our calculations for the variables above
+let totalDue = foodCost + tax + tip;
+
+//output the results to the console
+console.log("Food cost is " + foodCost + " and tax is " + tax);
+console.log("The tip is: " + tip);
+console.log("The total due is " + totalDue);
